7fd85daa76fe3cda152b7e70770a40d7f7902ac2
OmshriNT <61307578+OmshriNT@users.noreply.github.com>
2020-12-19 12:40:33 +0530

    final commit

--- script.js
+++ script.js
@@ -4,6 +4,46 @@ const blinkText = document.querySelector('.blink-me');
 const asideContent = document.querySelector('aside');
 const myMessage = document.createElement('p');
 
+const img = document.getElementById('carousel');
+const rightBtn = document.getElementById('right-btn');
+const leftBtn = document.getElementById('left-btn');
+
+
+let pictures = ['./images/snowboarding-in-nyc.png',
+    './images/car-in-snow.jpg',
+    './images/casey-neistat.png',
+    './images/Nyc_snow_2003_1.jpg',
+];
+
+
+// IMage carousel
+
+img.src = pictures[0];
+let position = 0;
+
+moveRight = () => {
+    if (position >= pictures.length - 1) {
+        position = 0;
+        img.src = pictures[position];
+        return;
+    }
+    img.src = pictures[position + 1];
+    position++;
+};
+
+moveLeft = () => {
+    if (position < 1) {
+        position = pictures.length - 1;
+        img.src = pictures[position];
+        return;
+    }
+    img.src = pictures[position - 1];
+    position--;
+}
+
+rightBtn.addEventListener('click', moveRight);
+leftBtn.addEventListener('click', moveLeft);
+
 // Like Button Animation and blinking text
 console.log(likeBtn);
 likeBtn.addEventListener('click', () => {
@@ -35,3 +75,4 @@ if (!localStorage.getItem('message')) {
 asideContent.appendChild(myMessage);
 
 promptBtn.addEventListener('click', setMessage);
+
