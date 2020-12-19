fecba03ee6cb0727fa4b0d8ab8b525769602107e
OmshriNT <61307578+OmshriNT@users.noreply.github.com>
2020-12-19 11:44:47 +0530

    commit

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 const likeBtn = document.querySelector('.like-btn');
 const promptBtn = document.querySelector('.prompt-btn');
+const blinkText = document.querySelector('.blink-me');
+const asideContent = document.querySelector('aside');
+const myMessage = document.createElement('p');
 
-// Like Button Animation
+// Like Button Animation and blinking text
 console.log(likeBtn);
 likeBtn.addEventListener('click', () => {
     likeBtn.classList.toggle('active');
+    blinkText.classList.toggle('active');
 });
 
 // Prompt box logic
+
+function setMessage() {
+
+    let message = prompt('Pour your thoughts!');
+
+    if (!message) {
+        setMessage();
+    } else {
+        localStorage.setItem('message', message);
+        myMessage.innerHTML = message;
+    }
+}
+
+if (!localStorage.getItem('message')) {
+    setMessage();
+} else {
+    let storedMessage = localStorage.getItem('message');
+    myMessage.innerHTML = storedMessage;
+}
+
+asideContent.appendChild(myMessage);
+
+promptBtn.addEventListener('click', setMessage);
